2788052c51b0a91eff688a7008fe7f336fa88198
Rebecca Vengen <rmvengen1@buffs.wtamu.edu>
2018-10-29 09:37:46 -0500

    Index View

--- LogInClient/Models/LogInViewModel.cs
+++ LogInClient/Models/LogInViewModel.cs
@@ -11,38 +11,21 @@ using System.Security.Claims;
 namespace LogInClient.Models
 {
 
-    public class Register
+    public class LogIn
     {
 
         [Required()]
         public string username { get; set;}
 
         [DataType(DataType.Password)]
-        [StringLength(255, MinimumLength = 8)]
+       
         [Required()]
         
         public string password { get; set;}
-        [Compare("Password")]
-        [DataType(DataType.Password)]
-        [StringLength(255, MinimumLength = 8)]
-        [Required()]
-    
-        public string confirmpassword {get; set;}
+         
 
+        
 
-        public string firstname {get; set;}
-
-        public string lastname {get; set;}
-
-        public string birthday {get; set;}
-
-        [DataType(DataType.EmailAddress)]
-        [StringLength(128)]
-        [Required()]
-        public string EmailAddress {get; set;}
-        [DataType(DataType.PhoneNumber)]
-        [RegularExpression("^[0-9]{8}$")]
-        [StringLength(32)]
-        public string phonenumber {get; set;}
+      
    }
 }
