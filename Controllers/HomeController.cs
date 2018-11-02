6d754cc91d3602e40dc0a44528c4836ed7490208
Rebecca Vengen <rmvengen1@buffs.wtamu.edu>
2018-11-02 11:50:03 -0500

    Added MVC outside of libraries

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using LogInLibrary.Models;
+using PRPC.Models;
 
 namespace PRPC.Controllers
 {
@@ -15,14 +15,14 @@ namespace PRPC.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
--- LogInLibrary/LogInProvider.cs
+++ LogInLibrary/LogInProvider.cs
@@ -6,24 +6,24 @@ namespace LogInLibrary
     {
         //This checks to see if the password matches the password from the database
         //Probably needs to specify where password comes fomr
-        public string PasswordVerify(string password)
+        /* public string PasswordVerify(string password)
         {
             if(inputtedPassword != password){
                 Console.WriteLine("Incorrect password, please try again");
             }else if(inputtedPassword == password){
                // Console.WriteLine("LogIn successful! Welcome {0}",username);
             }
-        }
+        }*/
         //This checks to see if the username and password match, 
         //needs to specify what username matches what password --> also 
         //need to be able to match username and password with rest of customer
         //information provided
-        public string LoginVerify (string username){
+        /* public string LoginVerify (string username){
             if(inputtedUsername == username && inputtedPassword == PasswordVerify){
                 Console.WriteLine("Login successful! Welcome {0}", username);
 
             }
         }
-
+*/
     }
 }
