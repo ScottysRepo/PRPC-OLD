c56dc03b9e6877501b184562d0e6ad8acc75962b
Rebecca Vengen <rmvengen1@buffs.wtamu.edu>
2018-11-07 14:28:31 -0600

    Change to Program.cs

--- LogInClient/Program.cs
+++ LogInClient/Program.cs
@@ -6,10 +6,12 @@ namespace LogInClient
 {
     class Program
     {
-        static void Main(string)
+        static void Main(string [] args)
         {
             LogInProvider provider = new LogInProvider();
-            Console.WriteLine(LogInProvider.PasswordVerify("abc123"));
+            Console.WriteLine("Please enter password");
+           // string inputtedPassword = Console.ReadLine();
+            Console.WriteLine(LogInProvider.PasswordVerify('abc123'));
             Console.WriteLine("Hello World!");
             //Console.WriteLine(PRPC.Method());
         }
